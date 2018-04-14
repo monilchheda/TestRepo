52bc8b4fcf712d6275638de4decda8de34cacfaa
monilchheda <monil@eliteral.com>
2018-04-14 13:37:40 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -10,11 +10,11 @@ echo "xfce4-session" > ~/.Xclients
 chmod a+x ~/.Xclients
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 systemctl start docker
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-yum install git
+yum install -y git
 cd
 mkdir nodeapp
 cd nodeapp
